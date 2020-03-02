d402454f4320a8570803ac165946e7b7c280c3d7
Josh Ellis <joshe523@gmail.com>
2020-03-02 15:03:15 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,5 @@
 def turn_count(board)
-  current_turn = 1
+  current_turn = 0
   board.each do |space| 
     space != " " && space != "" ? current_turn +=1 : current_turn
   end
@@ -7,5 +7,5 @@ def turn_count(board)
 end
 
 def current_player(board)
-  turn_count(board) % 2 == 0 ? "O" : "X"
+  turn_count(board) % 2 == 1 ? "O" : "X"
 end
